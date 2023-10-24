aef2c538dc0e66624b6cd81a4ce44bca1824e914
Ian Buchanan <ibuchanan@atlassian.com>
2023-10-24 11:15:03 -0500

    feat: basic error handling

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-const buildOutput = (rnd: number) => ({
-  body: '{"hello": "world"}',
-  headers: {
-    'Content-Type': ['application/json'],
-    'X-Request-Id': [`rnd-${rnd}`]
-  },
-  statusCode: 200,
-  statusText: 'OK'
-});
-
-exports.listener = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const result = buildOutput(Math.random());
-      resolve(result);
-    }, 1000);
-  });
+export function listener(req: any) {
+  try {
+    const body = JSON.parse(req.body);
+    console.log(body);
+    return {
+      body: '{"hello": "world"}',
+      headers: { "Content-Type": ["application/json"] },
+      statusCode: 200,
+      statusText: "OK",
+    };
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      const invalidJson = {
+        type: "about:blank",
+        title: "Invalid request",
+        detail: "The request must be valid JSON",
+      }
+      return {
+        body: JSON.stringify(invalidJson),
+        headers: { "Content-Type": ["application/problem+json"] },
+        statusCode: 400,
+        statusText: "Bad Request",
+      }
+    } else {
+      return {
+        body: error + "\n",
+        headers: { "Content-Type": ["application/json"] },
+        statusCode: 400,
+        statusText: "Bad Request",
+      }  
+    }
+  }
 };
